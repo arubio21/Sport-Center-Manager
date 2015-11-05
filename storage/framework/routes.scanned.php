e5f34b125e5e7680e5f81427a0366120f4f2192d
Álvaro Rubio <alvarorubio77@gmail.com>
2015-11-05 18:26:22 +0100

    CRUD

--- storage/framework/routes.scanned.php
+++ storage/framework/routes.scanned.php
@@ -1,1 +1,23 @@
-<?php 
+<?php $router->get('user', [
+	'uses' => 'scm\Http\Controllers\UserController@index',
+	'as' => NULL,
+	'middleware' => [],
+	'where' => [],
+	'domain' => NULL,
+]);
+
+$router->get('user/create', [
+	'uses' => 'scm\Http\Controllers\UserController@create',
+	'as' => NULL,
+	'middleware' => [],
+	'where' => [],
+	'domain' => NULL,
+]);
+
+$router->post('user/store', [
+	'uses' => 'scm\Http\Controllers\UserController@store',
+	'as' => NULL,
+	'middleware' => [],
+	'where' => [],
+	'domain' => NULL,
+]);
